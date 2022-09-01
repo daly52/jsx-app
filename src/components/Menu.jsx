c965d25fee23eaed136f3d3e799c2956dbb95bcc
daly52 <daly.affes@gmail.com>
2022-09-01 22:48:25 +0100

    my first commit

---
+++ src/components/Menu.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+  <>
+  <div style={{border:'solid 1px black',maxWidth:'100vw'}}>
+
+  <h1 className='titlered'>affes mohamed ali</h1>
+
+<br/>
+
+<img className='imageinsrc' src="/imageInSrc.jpg" alt='image1' />
+
+<br />
+
+<img src="/imageInPublic.jpg" alt='image2' />
+  </div>
+  
+
+
+
+
+
+<video width="320" height="240" controls>
+
+<source src="myVideo.mp4" type="video/mp4" />
+
+</video>
+  
+  </>
+  )
+};
+
+export default Menu
